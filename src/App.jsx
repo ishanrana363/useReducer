c5c6508388ec5ee1f28bf80d64e2bdca159bce85
mernishan <ishanrana500510@gmail.com>
2023-10-22 18:52:09 +0600

    useContext useReduce

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import ComponentA from "./component/ComponentA";
-import ComponentB from "./component/ComponentB";
+import { useReducer } from "react";
+import ComponentA from "./component/ComponentA"
+import ComponentB from "./component/ComponentB"
 import ComponentC from "./component/ComponentC"
-
+import React from "react";
+export const CountContext = React.createContext()
+const initialState = 0;
+const reducer = (state,action)=>{
+    switch(action){
+        case "increment":
+            return state+1;
+        case "decrement":
+            return state-1;
+        case "reset":
+            return initialState;
+        default:
+            return state
+    }
+}
 const App = () => {
-    
+    const [count,dispatch] = useReducer(reducer,initialState)
     return (
         <div>
-            <ComponentA/>
-            <ComponentB/>
-            <ComponentC/>
+            {/* <h1>Count : {count} </h1> */}
+            <CountContext.Provider value={{countState:count,countDispatch:dispatch}}>
+                <ComponentA/>
+                <ComponentB/>
+                <ComponentC/>
+            </CountContext.Provider>
         </div>
     );
 };
--- src/component/ComponentD.jsx
+++ src/component/ComponentD.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { CountContext } from "../App";
 const ComponentD = () => {
+    const countContext = useContext(CountContext)
     return (
         <div>
             ComponentD
+            <h1>Count value of D : {countContext.countState} </h1>
+        <button onClick={()=>{countContext.countDispatch("increment")}} >Increment</button>
+        <button onClick={()=>{countContext.countDispatch("decrement")}} >decrement</button>
+        <button onClick={()=>{countContext.countDispatch("reset")}} >reset</button>
         </div>
     );
 };
